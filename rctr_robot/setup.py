4ca11b49d2cc89cb57461e751a6810c3739a33d4
Alfaxad Eyembe <68440833+Alfaxad@users.noreply.github.com>
2025-07-17 22:13:33 +0900

    Update setup.py

--- rctr_robot/setup.py
+++ rctr_robot/setup.py
@@ -6,7 +6,7 @@ package_name = 'rctr_robot'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.2.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -17,19 +17,29 @@ setup(
         (os.path.join('share', package_name, 'config'),
             glob('config/*.yaml')),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'opencv-python',        # or opencv-python-headless
+        'ultralytics',          # YOLOv8 (pulls tqdm, pyyaml, etc.)
+        # torch & torchvision wheels are installed separately on the Pi
+    ],
     zip_safe=True,
-    maintainer='Team 11',
+    maintainer='Team 11',
     maintainer_email='team11@example.com',
-    description='RCTR Robot Control Package',
+    description='RCTR Robot Teleoperation & Perception Package',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'perception_node = rctr_robot.perception_node:main',
-            'control_node = rctr_robot.control_node:main',
-            'hardware_interface = rctr_robot.hardware_interface:main',
-            'challenge_manager = rctr_robot.challenge_manager:main',
+            # Robot‑side
+            'robot_hardware_interface = rctr_robot.robot_hardware_interface:main',
+            'robot_camera_streamer     = rctr_robot.robot_camera_streamer:main',
+            'robot_perception          = rctr_robot.robot_perception:main',
+            'robot_executor            = rctr_robot.robot_executor:main',
+
+            # Operator‑side
+            'operator_control = rctr_robot.operator_control:main',
+            'operator_display = rctr_robot.operator_display:main',
         ],
     },
 )
